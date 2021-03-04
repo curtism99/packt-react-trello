fd25125b54f214d1ada9638267dfdbbbcdf6a4d5
Curtis Mullins <curtismullins@gmail.com>
2021-03-04 01:19:41 -0600

    Created and inserted first components, including nested components.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import './components/Board';
+import Board from "./components/Board";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Board />
         <p>
-          I am learning React!
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
